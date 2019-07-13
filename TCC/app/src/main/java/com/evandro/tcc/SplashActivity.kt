289014fdc81515f0eab3de925119561059ff3099
santosevandro156 <48621567+santosevandro156@users.noreply.github.com>
2019-07-12 21:34:16 -0300

    Add files via upload

---
+++ TCC/app/src/main/java/com/evandro/tcc/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.evandro.tcc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        changeToLogin()
+    }
+
+    fun changeToLogin(){
+        val intent = Intent(this, LoginActivity::class.java)
+
+        Handler().postDelayed({
+            intent.change()
+        },2000)
+    }
+
+    fun Intent.change(){
+        startActivity(this)
+        finish()
+    }
+}
