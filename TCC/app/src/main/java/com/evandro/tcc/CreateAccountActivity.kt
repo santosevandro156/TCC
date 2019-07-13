83f28935997c4fb5a5553a72546ac30246b0779d
santosevandro156 <48621567+santosevandro156@users.noreply.github.com>
2019-07-13 18:56:20 -0300

    Add files via upload

--- TCC/app/src/main/java/com/evandro/tcc/CreateAccountActivity.kt
+++ TCC/app/src/main/java/com/evandro/tcc/CreateAccountActivity.kt
@@ -2,20 +2,17 @@ package com.evandro.tcc
 
 import android.app.ProgressDialog
 import android.content.Intent
-import android.nfc.Tag
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ProgressBar
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import kotlin.math.log
 
 class CreateAccountActivity : AppCompatActivity() {
 
@@ -129,7 +126,6 @@ class CreateAccountActivity : AppCompatActivity() {
             }
         }
     }
-
 
     //Directs user for main activity after insert information in database
     private fun updateUserAndInfoId(){
--- TCC/app/src/main/java/com/evandro/tcc/LoginActivity.kt
+++ TCC/app/src/main/java/com/evandro/tcc/LoginActivity.kt
@@ -1,17 +1,44 @@
 package com.evandro.tcc
 
+
+import android.app.ProgressDialog
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.support.annotation.RequiresApi
-import android.support.v4.content.ContextCompat
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import android.view.Window
 import android.view.WindowManager
 import android.os.Build
+import android.text.TextUtils
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_login.*
 
 
 class LoginActivity : AppCompatActivity() {
+
+    //Login element declaration
+    private var tvForgotPassword: TextView? = null
+    private var etEmail: TextView? = null
+    private var etPassword: TextView? = null
+    private var btnLogin: TextView? = null
+    private var btnCreateAccount: TextView? = null
+    private var mProgressBar: ProgressDialog? = null
+
+    //Database References
+    private var mAuth: FirebaseAuth? = null
+    private val TAG = "LoginActivity"
+
+    //Global variables
+    private var email: String? = null
+    private var password: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +47,9 @@ class LoginActivity : AppCompatActivity() {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
             window.setStatusBarColorTo(R.color.colorPrimary)
         }
+
+        //Call function: initialise
+        initialise()
     }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
@@ -30,5 +60,62 @@ class LoginActivity : AppCompatActivity() {
         this.statusBarColor = ContextCompat.getColor(baseContext, color)
     }
 
+    private fun initialise(){
+        //Find Values for variables
+        tvForgotPassword = findViewById<View>(R.id.tv_forgot_password) as TextView
+        etEmail = findViewById<View>(R.id.et_email) as EditText
+        etPassword = findViewById<View>(R.id.et_password) as EditText
+        btnLogin = findViewById<View>(R.id.btn_login) as Button
+        btnCreateAccount = findViewById<View>(R.id.btn_register_account) as Button
+        mProgressBar = ProgressDialog(this)
 
+        //Database Instances
+        mAuth               = FirebaseAuth.getInstance()
+
+        tvForgotPassword!!
+            .setOnClickListener{ startActivity(Intent(this@LoginActivity,
+                ForgotPasswordActivity::class.java))}
+
+        btnCreateAccount!!
+            .setOnClickListener{startActivity(Intent(this@LoginActivity,
+                CreateAccountActivity::class.java))}
+
+        btnLogin!!.setOnClickListener { loginUser() }
+    }
+
+    private fun loginUser(){
+        email = etEmail!!.text.toString()
+        password = etPassword!!.text.toString()
+
+        if(!TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)){
+            mProgressBar!!.setMessage("Checking User")
+            mProgressBar!!.show()
+
+            Log.d(TAG, "UserLogin")
+
+            mAuth!!.signInWithEmailAndPassword(email!!,password!!).addOnCompleteListener(this){
+                task ->
+
+                mProgressBar!!.hide()
+
+                //Authenticating user
+                if(task.isSuccessful){
+                    Log.d(TAG, "Login Success")
+                    //Call function: update user information
+                    updateUI()
+                }else{
+                    Log.d(TAG, "Login Failed", task.exception)
+                    Toast.makeText(this@LoginActivity, "Authentication failure", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }else{
+            Toast.makeText(this, "Fill all the information",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(){
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+    }
 }
